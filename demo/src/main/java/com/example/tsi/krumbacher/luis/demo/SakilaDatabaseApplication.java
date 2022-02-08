decbf1237b0a004bfc071bf45182c2a98280feda
Babelpoint Ltd <krumbacher@gmail.com>
2022-02-08 16:42:03 +0000

    update

---
+++ demo/src/main/java/com/example/tsi/krumbacher/luis/demo/SakilaDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.tsi.krumbacher.luis.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakilaDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SakilaDatabaseApplication.class, args);
+	}
+
+}
